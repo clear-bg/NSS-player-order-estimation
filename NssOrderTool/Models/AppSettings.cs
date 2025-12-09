6431703b3f7e37a8877dc96a66c81a1d37c619ed
clear-bg <clear7df_0719@icloud.com>
2025-12-09 18:24:26 +0900

    build: SSM接続設定をAppSettingsに追加

--- NssOrderTool/Models/AppSettings.cs
+++ NssOrderTool/Models/AppSettings.cs
@@ -1,13 +1,24 @@
 ﻿namespace NssOrderTool.Models
 {
-    // JSON設定ファイル用のクラス
-    public class AppConfig
-    {
-        public AppSettings? AppSettings { get; set; }
-    }
+  // JSON設定ファイル用のクラス
+  public class AppConfig
+  {
+    public AppSettings? AppSettings { get; set; }
+    public SsmSettings? SsmSettings { get; set; }
+  }
 
-    public class AppSettings
+  public class AppSettings
+  {
+    public string Environment { get; set; } = "TEST";
+  }
+
+  public class SsmSettings
     {
-        public string Environment { get; set; } = "TEST";
+        public bool UseSsm { get; set; } = false;
+        public string InstanceId { get; set; } = "";
+        public string RemoteHost { get; set; } = "";
+        public int RemotePort { get; set; } = 3306;
+        public int LocalPort { get; set; } = 3306;
+        public string AwsProfile { get; set; } = "default"; // 必要なら
     }
 }
