f117b60904c6703fc27a6110da1237966ae339e8
clear-bg <clear7df_0719@icloud.com>
2026-06-08 02:18:20 +0900

    feat: セッション履歴に任意の開催日時を記録・表示する機能を追加
    - ArenaSessionEntity に SessionDate カラムを追加しマイグレーションを適用
    - ArenaViewModel に年月日(YYYYMMDD)と時刻(HHMM)の入力欄を追加し、保存時に SessionDate として登録する処理を実装
    - ArenaRepository の履歴取得処理で、ソート順を SessionDate(降順)優先に変更
    - ArenaView のレイアウトを調整し、ラウンド入力とボタン群の間に開催日時の入力欄を配置

---
+++ NssOrderTool/Migrations/20260607165421_AddSessionDateToArenaSession.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NssOrderTool.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSessionDateToArenaSession : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "session_date",
+                table: "ArenaSessions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "session_date",
+                table: "ArenaSessions");
+        }
+    }
+}
